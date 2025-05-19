4bcc56ae5e5c817004409645434f5f64266dc2dd
Akinola Femi <akinolafemi573@gmail.com>
2025-05-19 07:06:11 +0100

    font-awesome error fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@
 import React from 'react'
 import Homepage from './Components/Home'
 import Cursor from './Components/Cursor'
-
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 const Home = () => {
   return (
     <Homepage>
